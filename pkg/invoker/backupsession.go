36ade6f8803783576a652c92b46767ee37dac005
Emruz Hossain <emruz@appscode.com>
2022-03-07 19:55:38 +0600

    Fix Pending phase calculation
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- pkg/invoker/backupsession.go
+++ pkg/invoker/backupsession.go
@@ -234,7 +234,7 @@ func calculateBackupSessionPhase(status *v1beta1.BackupSessionStatus) v1beta1.Ba
 		return v1beta1.BackupSessionFailed
 	}
 
-	if len(status.Targets) == 0 {
+	if len(status.Targets) == 0 || isAllTargetBackupPending(status.Targets) {
 		return v1beta1.BackupSessionPending
 	}
 
@@ -287,4 +287,13 @@ func upsertArray(cur, new []string) []string {
 		cur = append(cur, new[i])
 	}
 	return cur
+}
+
+func isAllTargetBackupPending(status []v1beta1.BackupTargetStatus) bool {
+	for _, t := range status {
+		if t.Phase != v1beta1.TargetBackupPending {
+			return false
+		}
+	}
+	return true
 }
