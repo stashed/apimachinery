23948a7e50e3bf0219842bf74af1e8bf8e72e0ba
Anisur Rahman <anisur@appscode.com>
2025-07-28 13:00:41 +0600

    fix lint
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- pkg/restic/unlock.go
+++ pkg/restic/unlock.go
@@ -20,6 +20,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"time"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -27,7 +29,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 	kutil "kmodules.xyz/client-go"
-	"time"
 )
 
 func (w *ResticWrapper) UnlockRepository() error {
